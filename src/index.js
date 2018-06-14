25f2d8226c33f3cb6cb1e86d3fdc85d51ecc32e6
Dan Pickett <dan.pickett@launchacademy.com>
2018-06-14 00:19:48 -0400

    set up exports for `index.js` file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { default as makeFixtureConfig } from './makeFixtureConfig'
 import { default as mockFetch } from './mockFetch'
 import { default as responseFromFixture } from './responseFromFixture'
+
+export {
+  makeFixtureConfig,
+  mockFetch,
+  responseFromFixture
+}
+
+export default mockFetch
